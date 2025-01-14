b774dcf4661fb04099c0accff04a3c19c05e2a85
raylanb <56618618+raylanb@users.noreply.github.com>
2025-01-14 17:07:15 +0800

    fix: Adapt to the status bar and navigation bar.

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:provider/provider.dart';
 import 'screens/timer_screen.dart';
 import 'providers/timer_provider.dart';
 import 'providers/theme_provider.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+
+  // 设置系统UI样式
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent, // 顶部状态栏背景色
+    statusBarIconBrightness: Brightness.light, // 顶部状态栏图标亮度
+    systemNavigationBarColor: Colors.transparent, // 导航栏背景色
+    systemNavigationBarIconBrightness: Brightness.light, // 导航栏图标亮度
+  ));
+
   runApp(
     MultiProvider(
       providers: [
@@ -23,22 +33,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<ThemeProvider>(
       builder: (context, themeProvider, child) {
+        // 根据主题设置状态栏和导航栏样式
+        SystemChrome.setSystemUIOverlayStyle(
+          themeProvider.isDark
+              ? SystemUiOverlayStyle.light.copyWith(
+                  statusBarColor: Colors.grey[800], // 深色主题状态栏颜色
+                  statusBarIconBrightness: Brightness.light, // 深色主题状态栏图标
+                  systemNavigationBarColor: Colors.grey[800],
+                  systemNavigationBarIconBrightness: Brightness.light,
+                )
+              : SystemUiOverlayStyle.dark.copyWith(
+                  statusBarColor: Colors.white, // 浅色主题状态栏颜色
+                  statusBarIconBrightness: Brightness.dark, // 浅色主题状态栏图标
+                  systemNavigationBarColor: Colors.white,
+                  systemNavigationBarIconBrightness: Brightness.dark,
+                ),
+        );
+
         return MaterialApp(
           title: '工作休息平衡计时器',
           theme: ThemeData(
             brightness: Brightness.light,
-            scaffoldBackgroundColor: Colors.white, // 白天使用白色背景
+            scaffoldBackgroundColor: Colors.white,
             textTheme: const TextTheme(
-              bodyLarge: TextStyle(color: Colors.black), // 设置默认文本颜色
+              bodyLarge: TextStyle(color: Colors.black),
               bodyMedium: TextStyle(color: Colors.black),
               bodySmall: TextStyle(color: Colors.black),
             ),
           ),
           darkTheme: ThemeData(
             brightness: Brightness.dark,
-            scaffoldBackgroundColor: Colors.grey[800], // 晚上使用灰色背景
+            scaffoldBackgroundColor: Colors.grey[800],
             textTheme: const TextTheme(
-              bodyLarge: TextStyle(color: Colors.white), // 设置默认文本颜色
+              bodyLarge: TextStyle(color: Colors.white),
               bodyMedium: TextStyle(color: Colors.white),
               bodySmall: TextStyle(color: Colors.white),
             ),
